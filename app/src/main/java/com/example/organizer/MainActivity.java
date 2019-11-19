5988717c7091c36e9e9c1d08d60bdca642dfdb20
Alexey Efimov <alexeyefimov15@gmail.com>
2019-11-19 17:14:48 +0200

    Fixed recyclerView

--- app/src/main/java/com/example/organizer/MainActivity.java
+++ app/src/main/java/com/example/organizer/MainActivity.java
@@ -10,29 +10,17 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import com.example.organizer.adapters.RVAdapter;
-import com.example.organizer.model.Note;
 import com.google.android.material.navigation.NavigationView;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.view.Window;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
-    private List<Note> notesList = new ArrayList<>();
-    private RecyclerView recyclerView;
-    private RVAdapter mAdapter;
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
@@ -56,69 +44,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupWithNavController(navigationView, navController);
 
         setColorBar();
-
-        recyclerView = findViewById(R.id.recycler_view);
-        mAdapter = new RVAdapter(notesList);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-
-        recyclerView.setAdapter(mAdapter);
-
-        prepareMovieData();
-
-    }
-
-    private void prepareMovieData() {
-        Note note = new Note("Mad Max: Fury Road", "Action & Adventure", "2015");
-        notesList.add(note);
-
-        note = new Note("Inside Out", "Animation, Kids & Family", "2015");
-        notesList.add(note);
-
-        note = new Note("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
-        notesList.add(note);
-
-        note = new Note("Shaun the Sheep", "Animation", "2015");
-        notesList.add(note);
-
-        note = new Note("The Martian", "Science Fiction & Fantasy", "2015");
-        notesList.add(note);
-
-        note = new Note("Mission: Impossible Rogue Nation", "Action", "2015");
-        notesList.add(note);
-
-        note = new Note("Up", "Animation", "2009");
-        notesList.add(note);
-
-        note = new Note("Star Trek", "Science Fiction", "2009");
-        notesList.add(note);
-
-        note = new Note("The LEGO Movie", "Animation", "2014");
-        notesList.add(note);
-
-        note = new Note("Iron Man", "Action & Adventure", "2008");
-        notesList.add(note);
-
-        note = new Note("Aliens", "Science Fiction", "1986");
-        notesList.add(note);
-
-        note = new Note("Chicken Run", "Animation", "2000");
-        notesList.add(note);
-
-        note = new Note("Back to the Future", "Science Fiction", "1985");
-        notesList.add(note);
-
-        note = new Note("Raiders of the Lost Ark", "Action & Adventure", "1981");
-        notesList.add(note);
-
-        note = new Note("Goldfinger", "Action & Adventure", "1965");
-        notesList.add(note);
-
-        note = new Note("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
-        notesList.add(note);
-
-        mAdapter.notifyDataSetChanged();
     }
 
     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
--- app/src/main/java/com/example/organizer/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/organizer/ui/home/HomeFragment.java
@@ -1,36 +1,94 @@
 package com.example.organizer.ui.home;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.organizer.R;
+import com.example.organizer.adapters.RVAdapter;
+import com.example.organizer.model.Note;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private RVAdapter rvAdapter;
+    private List<Note> notesList = new ArrayList<>();
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        //final TextView textView = root.findViewById(R.id.text_home);
 
-//        homeViewModel.getText().observe(this, new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
+        RecyclerView recyclerView = root.findViewById(R.id.recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        rvAdapter = new RVAdapter(notesList);
+
+        recyclerView.addItemDecoration(new DividerItemDecoration(Objects.requireNonNull(getContext()), LinearLayoutManager.VERTICAL));
+
+        recyclerView.setAdapter(rvAdapter);
+
+        prepareMovieData();
 
         return root;
     }
 
+    private void prepareMovieData() {
+        Note note = new Note("Mad Max: Fury Road", "Action & Adventure", "2015");
+        notesList.add(note);
 
+        note = new Note("Inside Out", "Animation, Kids & Family", "2015");
+        notesList.add(note);
+
+        note = new Note("Star Wars: Episode VII - The Force Awakens", "Action", "2015");
+        notesList.add(note);
+
+        note = new Note("Shaun the Sheep", "Animation", "2015");
+        notesList.add(note);
+
+        note = new Note("The Martian", "Science Fiction & Fantasy", "2015");
+        notesList.add(note);
+
+        note = new Note("Mission: Impossible Rogue Nation", "Action", "2015");
+        notesList.add(note);
+
+        note = new Note("Up", "Animation", "2009");
+        notesList.add(note);
+
+        note = new Note("Star Trek", "Science Fiction", "2009");
+        notesList.add(note);
+
+        note = new Note("The LEGO Movie", "Animation", "2014");
+        notesList.add(note);
+
+        note = new Note("Iron Man", "Action & Adventure", "2008");
+        notesList.add(note);
+
+        note = new Note("Aliens", "Science Fiction", "1986");
+        notesList.add(note);
+
+        note = new Note("Chicken Run", "Animation", "2000");
+        notesList.add(note);
+
+        note = new Note("Back to the Future", "Science Fiction", "1985");
+        notesList.add(note);
+
+        note = new Note("Raiders of the Lost Ark", "Action & Adventure", "1981");
+        notesList.add(note);
+
+        note = new Note("Guardians of the Galaxy", "Science Fiction & Fantasy", "2014");
+        notesList.add(note);
+
+        rvAdapter.notifyDataSetChanged();
+    }
 }
 
